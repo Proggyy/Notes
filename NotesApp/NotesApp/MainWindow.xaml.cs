1facef7dd574db4f156fee424d5c8653677bd681
Proggyy <126978795+Proggyy@users.noreply.github.com>
2023-03-04 23:34:34 +0400

    Add files via upload

---
+++ NotesApp/NotesApp/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+namespace NotesApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Передача контекста представлению
+            DataContext = new NotesViewModel(this, NotesList);
+        }
+    }
+}
---
+++ NotesApp/NotesApp/ViewModels/NotesViewModel.cs
@@ -0,0 +1,120 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Xml.Serialization;
+
+namespace NotesApp
+{
+    public class NotesViewModel
+    {
+        #region Приватные поля
+        string file = "notes.xml";
+        Window window;
+        ListView ListView;
+        BindingList<Note> notes;
+        XmlSerializer xmls = new XmlSerializer(typeof(BindingList<Note>));
+        #endregion
+
+        #region Команды
+        public ICommand CloseCommand { get; set; }
+        public ICommand AddCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+        public ICommand CheckCommand { get; set; }
+        public ICommand MinimizeCommand { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Конструктор модели представления
+        /// </summary>
+        /// <param name="window">Главное окно</param>
+        /// <param name="listView">Графический список элементов</param>
+        public NotesViewModel(Window window, ListView listView)
+        {
+            if (File.Exists(file))
+            {
+                notes = Deserialize();
+                File.Delete(file);
+            }
+            else
+            {
+                notes = new BindingList<Note>();
+            }
+            window.Closing += (sender, e) => Serialize();
+            this.window = window;
+            ListView = listView;
+            ListView.ItemsSource = notes;
+
+            CloseCommand = new Command(() =>
+            {
+                Serialize();
+                this.window.Close();
+            });
+            MinimizeCommand = new Command(() => window.WindowState = WindowState.Minimized);
+            DeleteCommand = new Command(() =>
+            {
+                if (ListView.SelectedIndex != -1)
+                {
+                    notes.RemoveAt(ListView.SelectedIndex);
+                }
+            });
+
+            AddCommand = new Command(() => notes.Add(new Note("new", "")));
+
+            CheckCommand = new Command(() => OpenNoteEditor());
+        }
+
+        #region Методы
+        /// <summary>
+        /// Открывает окно просмотра и редактирования
+        /// </summary>
+        private void OpenNoteEditor()
+        {
+            if (ListView.SelectedIndex == -1)
+            {
+                return;
+            }
+            CheckWindow checkWindow = new CheckWindow();
+            checkWindow.text.Text = notes[ListView.SelectedIndex].Header;
+            checkWindow.desc.Text = notes[ListView.SelectedIndex].NoteText;
+
+            checkWindow.SaveButton.Command = new Command(() =>
+            {
+                notes[ListView.SelectedIndex] = new Note(checkWindow.text.Text, checkWindow.desc.Text);
+                checkWindow.Close();
+            });
+            checkWindow.BackButton.Command = new Command(() => checkWindow.Close());
+
+            checkWindow.Show();
+        }
+
+        /// <summary>
+        /// Сериализует коллекцию элементов
+        /// </summary>
+        private void Serialize()
+        {
+            using (FileStream fs = new FileStream(file, FileMode.OpenOrCreate))
+            {
+                xmls.Serialize(fs, notes);
+            }
+        }
+
+        /// <summary>
+        /// Достает коллекцию элементов из xml документа
+        /// </summary>
+        /// <returns>Коллекция элементов из xml документа</returns>
+        private BindingList<Note> Deserialize()
+        {
+            BindingList<Note> DeserializedList = new BindingList<Note>();
+            using (FileStream fs = new FileStream(file, FileMode.OpenOrCreate))
+            {
+                DeserializedList = xmls.Deserialize(fs) as BindingList<Note>;
+            }
+            return DeserializedList;
+        }
+        #endregion
+    }
+}
