cf30b279a068587cfe1965526a01781a182be09e
Proggyy <126978795+Proggyy@users.noreply.github.com>
2023-03-06 01:26:13 +0400

    Add files via upload

---
+++ NotesApp/NotesApp/CheckWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace NotesApp
+{
+    /// <summary>
+    /// Логика взаимодействия для CheckWindow.xaml
+    /// </summary>
+    public partial class CheckWindow : Window
+    {
+        public CheckWindow()
+        {
+            InitializeComponent();
+            //Передача контекста представлению
+            DataContext = new NotesRedactorViewModel(this);
+        }
+    }
+}
---
+++ NotesApp/NotesApp/Model/Note.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NotesApp
+{
+    public class Note
+    {
+        /// <summary>
+        /// Заголовок заметки
+        /// </summary>
+        public string Header { get; set; }
+        /// <summary>
+        /// Дата создания заметки
+        /// </summary>
+        public DateTime Date { get; set; }
+        /// <summary>
+        /// Текст заметки
+        /// </summary>
+        public string NoteText { get; set; }
+
+        /// <summary>
+        /// Пустой конструктор для сериализации
+        /// </summary>
+        public Note() { }
+        /// <summary>
+        /// Конструктор заметки
+        /// </summary>
+        /// <param name="header">Заголовок</param>
+        /// <param name="noteText">Текст заметки</param>
+        public Note(string header, string noteText)
+        {
+            Header = header;
+            NoteText = noteText;
+            Date = DateTime.Now;
+        }
+    }
+}
