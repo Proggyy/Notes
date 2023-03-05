8e3131edf60b8d5c5f789ae85a9b067a21b96a7b
Proggyy <126978795+Proggyy@users.noreply.github.com>
2023-03-05 18:48:42 +0400

    Add files via upload

---
+++ NotesApp/NotesApp/bin/Debug/NotesApp/NotesApp/ViewModels/NotesRedactorViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+
+namespace NotesApp
+{
+    public class NotesRedactorViewModel
+    {
+        #region Приватные поля
+        Window window;
+        #endregion
+
+        #region Команды
+        public ICommand CloseCommand { get; set; }
+        public ICommand MinimizeCommand { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Конструктор модели представления
+        /// </summary>
+        /// <param name="window">Главное окно</param>
+        /// <param name="listView">Графический список элементов</param>
+        public NotesRedactorViewModel(Window window)
+        {
+            this.window = window; 
+
+            CloseCommand = new Command(() => this.window.Close());
+            MinimizeCommand = new Command(() => window.WindowState = WindowState.Minimized);
+        }
+    }
+}
